357b5a99c3b53e7c72125cd96f8984e9b15e92fb
Orlando Melo <42143701+NandoMelo07@users.noreply.github.com>
2020-09-11 14:13:01 -0300

    Add files via upload

---
+++ newboootstrap/java.js
@@ -0,0 +1,45 @@
+function mouse(){
+    var paragrafo = document.getElementById('paragrafo');
+
+    paragrafo.style.color = "blue";
+}
+
+function ideias(){
+    var primeiro = document.getElementById("primeiro");
+    primeiro.style.color = "blue";
+
+}
+
+function objetivo(){
+    var segundo = document.getElementById("segundo");
+    segundo.style.color = "blue";
+}
+
+function valores(){
+    var terceiro = document.getElementById("terceiro");
+    terceiro.style.color = "blue";
+
+}
+
+function segurança(){
+    var quarto = document.getElementById("quarto");
+    quarto.style.color = "blue";
+
+}
+
+function nando(){
+    var biografia = document.getElementById("biografia");
+    biografia.style.color = "blue";
+}
+
+function fotos(){
+    var galeria = document.getElementById("galeria");
+    galeria.style.color = "blue";
+}
+
+function site(){
+    var inicio = document.getElementById("inicio");
+    inicio.style.color = "blue";
+}
+
+//Daqui pra frente eu vou montar uma galeria.
